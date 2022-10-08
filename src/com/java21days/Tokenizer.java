b8f3ab9590a29ad8ff44e64ce9368fa2c1e788d4
Alejandro Salas <asalas.wd@outlook.com>
2022-10-08 12:16:47 -0400

    Display number of tokens and then display all tokens

--- src/com/java21days/Tokenizer.java
+++ src/com/java21days/Tokenizer.java
@@ -1,7 +1,19 @@
 package com.java21days;
 
+import java.util.StringTokenizer;
+
 public class Tokenizer {
     public static void main(String[] args) {
-        System.out.println("This is the tokenizer method");
+        StringTokenizer tokened1, tokened2;
+
+        String str1 = "A Lannister always pays his debts.";
+        String str2 = "Herecutiiscutiacutisentencecutiincutisecretcuticodecuti.";
+
+        tokened1 = new StringTokenizer(str1);
+        int tokenCount1 = tokened1.countTokens();
+        System.out.println("The first phrase has " + tokenCount1 + " tokens");
+        for (int i=1; i<=tokenCount1; i++) {
+            System.out.println("Token " + i + ": " + tokened1.nextToken());
+        }
     }
 }
