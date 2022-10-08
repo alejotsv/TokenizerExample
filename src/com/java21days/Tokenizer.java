896d6683474e536238b24627ae434533179f7872
Alejandro Salas <asalas.wd@outlook.com>
2022-10-08 12:42:00 -0400

    Create method that accepts a String and one that accepts a String and a token

--- src/com/java21days/Tokenizer.java
+++ src/com/java21days/Tokenizer.java
@@ -7,9 +7,10 @@ public class Tokenizer {
         StringTokenizer tokened1, tokened2;
 
         String str1 = "A Lannister always pays his debts.";
-        String str2 = "Herecutiiscutiacutisentencecutiincutisecretcuticodecuti.";
+        String str2 = "Here/is/another/phrase.";
 
         showTokens(str1);
+        showTokens(str2, "/");
     }
 
     static void showTokens(String str) {
@@ -22,8 +23,8 @@ public class Tokenizer {
         }
     }
 
-    static void showTokens(String str, String token) {
-        StringTokenizer stk = new StringTokenizer(str, token);
+    static void showTokens(String str, String delim) {
+        StringTokenizer stk = new StringTokenizer(str, delim);
         int tokenCount = stk.countTokens();
 
         System.out.println("The phrase '" + str + "' has " + tokenCount + " tokens:");
